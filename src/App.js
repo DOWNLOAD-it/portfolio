6975f242fd1e32a9d2194fb839faa322607c8898
HASSANI Abderrahmane <abderrahmane.hassani123@gmail.com>
2024-04-27 00:19:44 +0100

    modified:   package-lock.json modified:   package.json new file:   src/App.css modified:   src/App.js modified:   src/components/Projects.jsx new file:   src/components/images/background/1.jpg 	new file:   src/components/images/background/2.jpg modified:   src/components/styles/Hero.css new file:   src/components/styles/Projects.css modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className="app-container">
       <Hero />
       <Projects />
-    </>
+    </div>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,40 @@
 import projectImage from "./images/projects/1.jpg";
+import "./styles/Projects.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faLaravel } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
   return (
     <div className="projects-container">
       <div className="project-card">
+        <div className="card-content">
+          <h1 className="project-name">Best gaming site ever</h1>
+          <div className="project-techstack">
+            <FontAwesomeIcon icon={faReact} />
+            <FontAwesomeIcon icon={faLaravel} />
+          </div>
+        </div>
         <img src={projectImage} alt="" />
-        <div></div>
+      </div>
+      <div className="project-card">
+        <div className="card-content">
+          <h1 className="project-name">Best gaming site ever</h1>
+          <div className="project-techstack">
+            <FontAwesomeIcon icon={faReact} />
+            <FontAwesomeIcon icon={faLaravel} />
+          </div>
+        </div>
+        <img src={projectImage} alt="" />
+      </div>
+      <div className="project-card">
+        <div className="card-content">
+          <h1 className="project-name">Best gaming site ever</h1>
+          <div className="project-techstack">
+            <FontAwesomeIcon icon={faReact} />
+            <FontAwesomeIcon icon={faLaravel} />
+          </div>
+        </div>
+        <img src={projectImage} alt="" />
       </div>
     </div>
   );
